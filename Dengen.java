bbf64b270671e28dd3eefbfc94a924b267f4cb3c
SasoReon <88305983+SasoReon@users.noreply.github.com>
2021-08-06 13:30:13 +0900

    Add files via upload

---
+++ Dengen.java
@@ -0,0 +1,5 @@
+package kadai15;
+
+abstract class dengen { // Targetクラス
+    public abstract int kyuuden(); // request()メソッド
+}
---
+++ JapaneseConsent.java
@@ -0,0 +1,7 @@
+package kadai15;
+
+public class JapaneseConsent {
+    public int power() {
+        return 100;
+    }
+}
